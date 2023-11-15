2244e54bacaf80191735b0ff9cadf63b8cabad6e
aakzsh <aakashferrari@gmail.com>
2023-11-15 12:35:17 +0530

    change dir structure setup backend

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/check')
+def check():
+    return "check"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
